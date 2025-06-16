1fb4aa788b98c36c3e654abfef2bb999390582c5
RajanMajhi <rajanmajhi117@gmail.com>
2025-06-16 18:30:56 +0545

    form verfied

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
       ),
-      home: const MyHomePage(),
+      home: const Registrationpage(),
     );
   }
 }
